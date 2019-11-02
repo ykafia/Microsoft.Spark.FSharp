d26b13a2d915dbbe9eb2977a4d5e1a5fa3d84468
Youness KAFIA <kafia.youness@gmail.com>
2019-11-02 14:46:59 +0100

    More wrapping with F#

--- Library.fs
+++ Library.fs
@@ -1,8 +1,8 @@
 ﻿namespace Microsoft.Spark.FSharp
 open Microsoft.Spark.Sql
 
-module Column =
-    let Col (name:string) = 
+module SparkCol =
+    let Col (name:string)  = 
         Functions.Col(name)
     let Split (pattern : string) (column: Column) : Column =
         Functions.Split(column,pattern)
@@ -11,7 +11,42 @@ module Column =
     let SplitAlias (pattern : string) (name:string) (nameCol:string) =
         Split pattern (Col nameCol)
             |> Alias name
+    let Explode (nameCol : string)  =
+        Col nameCol
+            |> Functions.Explode
+    
 
-module DF = 
+module SparkDF = 
+    let Alias (name:string) (df:DataFrame)=
+        name |>
+            df.Alias
+    let As (name:string) (df:DataFrame) =
+        name |> df.As
+
+    let Checkpoint (eager:bool) (df:DataFrame) : DataFrame = 
+        df.Checkpoint eager
+
+    let Coalesce (number:int) (df:DataFrame) =
+        number |> df.Coalesce
+
     let Select (col:list<Column>) (df:DataFrame) : DataFrame= 
         df.Select(Array.ofList col)
+    
+
+    // let x (df:DataFrame) = 
+    //     df.Col
+    //     0
+module SparkFunc = 
+    let Col (name:string) = Functions.Col name
+    let CollectList (col:Column) = Functions.CollectList col
+    let CollectListByName (colName:string) = 
+        Col colName 
+            |> CollectList
+    let CollectSet (col:Column) = Functions.CollectSet col
+    let CollectSetByName (colName:string) = 
+        Col colName 
+            |> CollectSet
+    let Column = Col
+
+    // let x = Functions.
+    
