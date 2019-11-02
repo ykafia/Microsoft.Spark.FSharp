a175c2c1c866c023c87f652fb03ec2b478c18efd
Youness KAFIA <kafia.youness@gmail.com>
2019-11-02 14:15:41 +0100

    Initial commit

---
+++ Library.fs
@@ -0,0 +1,17 @@
+﻿namespace Microsoft.Spark.FSharp
+open Microsoft.Spark.Sql
+
+module Column =
+    let Col (name:string) = 
+        Functions.Col(name)
+    let Split (pattern : string) (column: Column) : Column =
+        Functions.Split(column,pattern)
+    let Alias (name:string) (col:Column) =
+        col.Alias(name)
+    let SplitAlias (pattern : string) (name:string) (nameCol:string) =
+        Split pattern (Col nameCol)
+            |> Alias name
+
+module DF = 
+    let Select (col:list<Column>) (df:DataFrame) : DataFrame= 
+        df.Select(Array.ofList col)
